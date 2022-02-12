d24ffd9580bf9625c649a692da0e6c1af2a0f070
lincoln auster [they/them] <lincolnauster@gmail.com>
2022-02-12 11:07:00 -0700

    store enums and flags in enums instead of macros
    In C, enums begin at 0 and increment upwards. Stylistic name changes
    aside, the behavior is identical to the #define blocks, but the compiler
    is better able to type-check our work.
    
    This commit also apparently adds in a newline at the end of register.h,
    which should be the case in every file so that the number of newlines
    directly corresponds with the number of lines in general.

--- c/register.h
+++ c/register.h
@@ -1,27 +1,29 @@
 #ifndef REGISTER_H
 #define REGISTER_H
 
-#define rax 0x0;
-#define rcx 0x1;
-#define rdx 0x2;
-#define rbx 0x3;
-#define rsp 0x4;
-#define rbp 0x5;
-#define rsi 0x6;
-#define rdi 0x7;
-#define r8 0x8;
-#define r9 0x9;
-#define r10 0xA;
-#define r11 0xB;
-#define r12 0xC;
-#define r13 0xD;
-#define r14 0xE;
-#define noreg 0xF;
-#define RegSize = 15;
+enum reg {
+	RAX = 0x0,
+	RCX,
+	RDX,
+	RBX,
+	RSP,
+	RSI,
+	RDI,
+	R8,
+	R9,
+	R10,
+	R11,
+	R12,
+	R13,
+	R14,
+	RNONE,
+};
 
-#define ZF 0x2;
-#define SF 0x1;
-#define OF 0x0;
+enum flags {
+	OF = 0x0,
+	SF,
+	ZF,
+}
 
 int getReg(int regNum);
 void storeReg(int regNum, int value);
