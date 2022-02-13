3ab7abab227f6e9c7ca78f19b14cfaf1ded9f7d8
codeing0-0 <Hi0ai0imir@gmail.com>
2022-02-13 19:41:17 +0530

    Completed Memory.c

---
+++ c/memory.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <memory.h>
+#include "memory.h"
+#include "tools.h"
+
+static int memory[MEMSIZE];
+
+void clearMemory() {
+    memset(memory, 0, MEMSIZE * sizeof(*memory));
+}
+
+void store(int address, int value, bool * memError) {
+    if (address < 0 || address > MEMSIZE) {
+        (*memError) = true;
+    } else {
+        (*memError) = false;
+        memory[address] = value;
+    }
+}
+
+int fetch(int address, bool * memError) {
+    if (address < 0 || address > MEMSIZE) {
+        (*memError) = true;
+    } else {
+        (*memError) = false;
+        return memory[address];
+    }
+}
+
+char getByte(int address, bool * memError) {
+    if (address < 0 || address > MEMBYTE) {
+        (*memError) = true;
+        return 0;
+    } else {
+        (*memError) = false;
+        int w_addr = fetch((address / 4), memError);
+        char * addr = (char*)&w_addr;
+        char retn = addr[(address % 4)];
+        return retn;
+    }
+}
+
+void setByte(int address, char value, bool * memError) {
+    if (address < 0 || address > MEMBYTE) {
+        (*memError) = true;
+    } else {
+        (*memError) = false;
+        int w_addr = fetch((address / 4), memError);
+        char * addr = (char*)&w_addr;
+        addr[(address % 4)] = value;
+        store(address / 4, w_addr, memError);
+    }
+}
+
+int getWord(int address, bool * memError) {
+    if (address < 0 || address > MEMBYTE || (address % 4) != 0) {
+        (*memError) = true;
+        return 0;
+    } else {
+        (*memError) = false;
+        return fetch(address / 4, memError);
+    }
+}
+
+void setWord(int address, int value, bool * memError) {
+    if (address < 0 || address > MEMBYTE || (address % 4) != 0) {
+        (*memError) = true;
+    } else {
+        (*memError) = false;
+        store((address / 4), value, memError);
+    }
+}
