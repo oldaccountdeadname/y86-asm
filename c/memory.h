303f03d446965f42d10fc79ba68c6646478028f9
codeing0-0 <Hi0ai0imir@gmail.com>
2022-02-13 19:23:52 +0530

    Finished memory.h

--- c/memory.h
+++ c/memory.h
@@ -1,6 +1,10 @@
 #ifndef MEMORY_H
 #define MEMORY_H
-
-
-
+#define MEMSIZE 1024    //1024 words of memory
+#define MEMBYTE 4095
+char getByte(int address, bool * memError);
+void setByte(int address, unsigned char value, bool * memError);
+void clearMem();
+int getWord(int address, bool * memError);
+void setWord(int address, unsigned int value, bool * memError);
 #endif
