330d188da2d92901020597fc0a058bb6579523d6
lincoln auster [they/them] <lincolnauster@gmail.com>
2022-02-16 17:01:53 -0700

    Revert "completed register.c"
    This reverts commit 3048b0edd944ad6d0e172ef55c3a0974f71b134c, and uses
    an enum instead of preprocessor macros for the reasons outlined in
    d24ffd9.

--- c/register.h
+++ c/register.h
@@ -1,27 +1,23 @@
-#define rax 0x0
-#define rcx 0x1
-#define rdx 0x2
-#define rbx 0x3
-#define rsp 0x4
-#define rbp 0x5
-#define rsi 0x6
-#define rdi 0x7
-#define r8 0x8
-#define r9 0x9
-#define r10 0xA
-#define r11 0xB
-#define r12 0xC
-#define r13 0xD
-#define r14 0xE
-#define RNONE  0xf
-#define REGSIZE 15
-#define ZF     0x2
-#define SF     0x1
-#define OF     0x0
+enum reg {
+	RAX = 0x0,
+	RCX,
+	RDX,
+	RBX,
+	RSP,
+	RSI,
+	RDI,
+	R8,
+	R9,
+	R10,
+	R11,
+	R12,
+	R13,
+	R14,
+	RNONE,
+};
 
-int get_reg(int regNum);
-void set_reg(int regNum, unsigned int regValue);
-void clear_reg(void);
-void clear_cc(void);
-void set_cc(unsigned int bitNumber, unsigned int value);
-int get_cc(unsigned int bitNumber);
+enum flags {
+	OF = 0x0,
+	SF,
+	ZF,
+};
