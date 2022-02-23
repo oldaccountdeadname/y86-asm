ee5e2dee49364d1432efdaffc3ad4b1ce8851631
lincoln auster [they/them] <lincolnauster@gmail.com>
2022-02-23 12:32:15 -0700

    tests: fix typo in runner output

--- tests/runner.sh
+++ tests/runner.sh
@@ -3,6 +3,6 @@
 for case in *.s; do
 	y86-asm "$case" --output "$case.actual"
 	diff "$case.expected" "$case.actual" || { echo "$case failed."; exit 1; }
-	echo -e "\033[1;32m$case succeded\033[0m"
+	echo -e "\033[1;32m$case succeeded\033[0m"
 	rm $case.actual
 done
