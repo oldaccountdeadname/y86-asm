1b38e3ce4a9529291cc503f6d0f4657f73e48553
lincoln auster [they/them] <lincolnauster@gmail.com>
2022-03-06 08:05:20 -0700

    repo: remove unused files

--- src/memory.h
+++
@@ -1,8 +0,0 @@
-#define MEMSIZE 1024    //1024 words of memory
-#define MEMBYTE 4095
-
-char get_byte(int address, bool *memError);
-void set_byte(int address, unsigned char value, bool *memError);
-void clear_mem(void);
-int get_word(int address, bool *mem_err);
-void set_word(int address, unsigned int value, bool *mem_err);
